212b496c957a716e09c5ed2613eb2a99b9bbee91
omekrit <khomkrit.y@gmail.com>
2021-08-04 21:30:58 +0700

    init user routers api

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/uses/currentuser', () => {});
+router.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi');
+});
 
 export { router as currentUserRouter };

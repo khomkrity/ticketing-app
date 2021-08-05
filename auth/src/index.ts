a5f08168f726668affca7012ed92ab040ba355ce
omekrit <khomkrit.y@gmail.com>
2021-08-05 14:12:12 +0700

    init error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,14 +3,19 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(express.json());
 
+// user routes
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// error handler middleware
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!!');

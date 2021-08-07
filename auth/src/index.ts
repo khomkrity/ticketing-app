89174a3f9f0f61b90879637e6a195d6b35d8b32d
omekrit <khomkrit.y@gmail.com>
2021-08-07 13:03:02 +0700

    add db connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
@@ -23,6 +24,21 @@ app.all('*', () => {
 // error handler middleware
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!');
+  });
+};
+
+start();

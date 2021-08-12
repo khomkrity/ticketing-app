0504e878a2e96124468e92e259ea91484b26c3f8
omekrit <khomkrit.y@gmail.com>
2021-08-12 15:17:12 +0700

    add non-authenticated test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -7,7 +7,13 @@ const { currentuser } = endPoints;
 it('responds with details about the current user', async () => {
   const cookie = await global.signin();
 
-  const response = await request(currentuser).get(currentuser).set('Cookie', cookie).expect(200);
+  const response = await request(app).get(currentuser).set('Cookie', cookie).expect(200);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app).get(currentuser).expect(200);
+
+  expect(response.body.currentUser).toBeNull();
 });

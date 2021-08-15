3c47af6e9c3b813a0139ce4b7bb9192ef1e9f15b
omekrit <khomkrit.y@gmail.com>
2021-08-15 12:30:48 +0700

    add error validation

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,13 +1,23 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 const signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
-  const onSubmit = event => {
+  const onSubmit = async event => {
     event.preventDefault();
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
 
-    console.log(email, password);
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -26,6 +36,16 @@ const signup = () => {
           className='form-control'
         />
       </div>
+      {errors.length > 0 && (
+        <div className='alert alert-danger'>
+          <h4>Oops...</h4>
+          <ul className='my-0'>
+            {errors.map(err => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className='btn btn-primary'>Sign Up</button>
     </form>
   );

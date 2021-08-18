890ac3d7ebf535d6dc4ef790be1118520223e809
omekrit <khomkrit.y@gmail.com>
2021-08-18 13:42:51 +0700

    add comments

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const buildClient = ({ req }) => {
+  // check if the method is executed in server or client
   if (typeof window === 'undefined') {
     // server rendering
-
     return axios.create({
       baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers,

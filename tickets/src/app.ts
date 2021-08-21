b7466a44fabc1661dec9e4e44f70db154d067892
omekrit <khomkrit.y@gmail.com>
2021-08-21 15:22:28 +0700

    add require auth and current user middleware

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@omekrit-ticketing/common';
+import { errorHandler, NotFoundError, currentUser } from '@omekrit-ticketing/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -12,6 +13,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all('*', () => {
   throw new NotFoundError();

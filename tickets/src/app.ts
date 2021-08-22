65cbe5e46ae203627bc46a324b3d9a32e784d408
omekrit <khomkrit.y@gmail.com>
2021-08-22 17:24:52 +0700

    add get ticket router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@omekrit-ticketing/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,6 +16,7 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', () => {
   throw new NotFoundError();

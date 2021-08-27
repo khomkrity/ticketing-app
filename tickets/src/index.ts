28021f9c20c993b54797114866c9609a5f928be2
omekrit <khomkrit.y@gmail.com>
2021-08-27 14:21:03 +0700

    add nats client instance

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   try {
@@ -10,7 +11,7 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
       throw new Error('MONGO_URI must be defined');
     }
-
+    await natsWrapper.connect('ticketing', 'randomId', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,

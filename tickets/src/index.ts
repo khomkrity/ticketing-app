7939ebdc677c862222acc7aa5cf09afc08524afb
omekrit <khomkrit.y@gmail.com>
2021-08-27 21:02:23 +0700

    add env for nats

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,7 +11,24 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
       throw new Error('MONGO_URI must be defined');
     }
-    await natsWrapper.connect('ticketing', 'randomId', 'http://nats-srv:4222');
+
+    if (!process.env.NATS_CLUSTER_ID) {
+      throw new Error('NATS_CLUSTER_ID must be defined');
+    }
+
+    if (!process.env.NATS_CLIENT_ID) {
+      throw new Error('NATS_CLIENT_ID must be defined');
+    }
+
+    if (!process.env.NATS_URL) {
+      throw new Error('NATS_URL must be defined');
+    }
+
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed');
       process.exit();

c01ddb6ba545c28e8bc351b072d18de57b856040
omekrit <khomkrit.y@gmail.com>
2021-08-27 11:31:03 +0700

    add sub class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -16,22 +16,7 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('orders-service');
-  const subscription = stan.subscribe('ticket:created', 'orders-queue-group', options);
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -74,5 +59,16 @@ abstract class Listener {
   parseMessage(msg: Message) {
     const data = msg.getData();
     return typeof data === 'string' ? JSON.parse(data) : JSON.parse(data.toString('utf-8'));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('event data', data);
+
+    msg.ack();
   }
 }

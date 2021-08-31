17142dc1eeb4ce127278f06de34c36a6c00f2884
omekrit <khomkrit.y@gmail.com>
2021-08-31 13:27:13 +0700

    add order cancelling logic

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,27 @@
 import express, { Request, Response } from 'express';
+import { Order, OrderStatus } from '../models/orders';
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@omekrit-ticketing/common';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+// could be patch or put
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+
+  const order = await Order.findById(orderId);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  res.send(order);
 });
 
 export { router as deleteOrderRouter };

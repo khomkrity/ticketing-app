6d77648151303903f8e72e39ee078bba4d7140f2
omekrit <khomkrit.y@gmail.com>
2021-09-01 10:58:51 +0700

    add const for queue group name

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,10 +1,11 @@
 import { Message } from 'node-nats-streaming';
 import { Subjects, Listener, TicketCreatedEvent } from '@omekrit-ticketing/common';
 import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
 }

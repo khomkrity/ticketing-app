ea3f2bf4ef72e9c085ed8bb0beec478343c5a4e0
omekrit <khomkrit.y@gmail.com>
2021-09-01 10:53:09 +0700

    init ticket:created listener

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,10 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketCreatedEvent } from '@omekrit-ticketing/common';
+import { Ticket } from '../../models/ticket';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+  queueGroupName = 'orders-service';
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+}

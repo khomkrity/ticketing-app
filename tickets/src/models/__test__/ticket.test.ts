727a2957b1f92a44389a26f67e20216f4a024b8a
omekrit <khomkrit.y@gmail.com>
2021-09-01 16:17:18 +0700

    init ticket versioning test

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,28 @@
+import { Ticket } from '../tickets';
+
+it('implements optimistic concurrency control', async () => {
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+
+  // save the ticket into the database
+  await ticket.save();
+
+  // fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // make two seprarate changes to the fetched tickets
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 20 });
+
+  // save the first fetched ticket
+  await firstInstance!.save();
+
+  // save the second fetched ticket
+  await secondInstance!.save();
+  // expect to see versioning error
+});

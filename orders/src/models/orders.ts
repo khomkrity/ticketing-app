49a4034eac73599d1af962d4f89e7c0f7354c1f4
omekrit <khomkrit.y@gmail.com>
2021-09-02 13:06:25 +0700

    add optimistic concurrency to order model

--- orders/src/models/orders.ts
+++ orders/src/models/orders.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@omekrit-ticketing/common';
 import { TicketDoc } from './ticket';
 
@@ -53,6 +54,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin, { strategy: 'version' });
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);

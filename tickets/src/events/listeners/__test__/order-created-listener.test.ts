f2ae2abe9ed7eca347be0441f5607e881bc72581
omekrit <khomkrit.y@gmail.com>
2021-09-06 12:22:02 +0700

    init order created listner test

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,58 @@
+import { OrderCreatedListener } from '../order-created-listener';
+import { OrderCreatedEvent, OrderStatus } from '@omekrit-ticketing/common';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../../models/tickets';
+import mongoose from 'mongoose';
+
+const setupMockOrderEvent = async () => {
+  // create an instance of the listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // create and save a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 99,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  });
+  await ticket.save();
+
+  // create a fake data event
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    expiresAt: 'random',
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+    version: 0,
+  };
+
+  // create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
+
+it('reserves the ticket by setting orderId to that ticket', async () => {
+  const { listener, ticket, data, msg } = await setupMockOrderEvent();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.orderId).toEqual(data.id);
+});
+
+it('acknowledges the message', async () => {
+  const { listener, data, msg } = await setupMockOrderEvent();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});

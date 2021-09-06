78121e30dcfba513820207a7dd9040d1fce6eb91
omekrit <khomkrit.y@gmail.com>
2021-09-06 12:06:20 +0700

    add order listener logic

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,28 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@omekrit-ticketing/common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/tickets';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // if no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // save the ticket
+    await ticket.save();
+
+    // acknowledge the message
     msg.ack();
   }
 }

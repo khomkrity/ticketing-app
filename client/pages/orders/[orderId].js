4340041d2c3319f484a0e3a8732be15654799c9f
omekrit <khomkrit.y@gmail.com>
2021-09-12 12:35:42 +0700

    add user's order page

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  // call it once the component is rendered to calculate expiration
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const milliSecondLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(milliSecondLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return <div>Time left to pay: {timeLeft} seconds</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
